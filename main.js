5d7696f7967858069c8bfb22ab94863743749cb0
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-03-07 18:05:03 -0600

    added object

--- main.js
+++ main.js
@@ -3,3 +3,28 @@
  //   selectedDiv.innerHTML += textToPrint;
  
  console.log('hi');
+
+ const projects = [
+     {title: "Cool Project",
+     screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+     description: "This is the cool project",
+     technologiesUsed: "HTML, CSS, Vanilla JavaScript",
+     
+
+
+     }
+
+    
+     {
+
+
+     }
+     {
+
+
+     }
+
+
+
+
+ ]
