6c8bd930bf627e1a92fc270cac90fda2f98f01f7
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-06-22 13:55:15 -0500

    adding image

---
+++ src/javascripts/helpers/data/projectsData.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+
+const getProjects = () => new Promise((resolve, reject) => {
+  axios.get('https://personal-bio-site-a40da.firebaseio.com/projects.json')
+    .then((results) => {
+      const projectResults = results.data;
+      const projects = [];
+      Object.keys(projectResults).forEach((projectId) => {
+        projectResults[projectId].id = projectId;
+        projects.push(projectResults[projectId]);
+      });
+      resolve(projects);
+    })
+    .catch(err => reject(err));
+});
+
+export default { getProjects };
