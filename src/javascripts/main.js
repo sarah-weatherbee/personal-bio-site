38488970a1ca8674796a941add89f59ffeb2bf68
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-08-25 16:32:29 -0500

    added clips, fixed footer and images

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,7 @@
 import '../styles/main.scss';
 import 'bootstrap';
 import projectsData from './helpers/data/projectsData';
+import clipsData from './helpers/data/clipsData';
 
 
 const printToDom = (divId, textToPrint) => {
@@ -12,10 +13,10 @@ const createProjectCards = (projects) => {
   let domString = '<div class="lead"><h3 class="lead">Development</h3></div>';
   domString += '<div class="newRow">';
   for (let i = 0; i < projects.length; i += 1) {
-    domString += '<div class="col-12 col-md-6 col-lg-4 p-4">';
+    domString += '<div class="col-12 col-md-6 col-lg-5 p-4">';
     domString += '<div class="card d-flex h-100">';
-    domString += `<div class="card-header"><strong>${projects[i].Name}</strong></div>`;
     domString += `<div class="card-img-top"><img class="image" src=${projects[i].imageUrl} alt="project picture"></div>`;
+    domString += `<div class="card-header"><strong>${projects[i].Name}</strong></div>`;
     domString += '<div class="card-body">';
     domString += `<div>${projects[i].description}</div>`;
     domString += '<ul class="list-group">';
@@ -31,6 +32,27 @@ const createProjectCards = (projects) => {
   printToDom('projectsPage', domString);
 };
 
+const createClipCards = (clips) => {
+  let domString = '<div class="lead"><h3 class="lead">Journalism</h3></div>';
+  domString += '<div class="newRow">';
+  for (let j = 0; j < clips.length; j += 1) {
+    domString += '<div class="col-12 col-md-6 col-lg-5 p-4">';
+    domString += '<div class="card d-flex h-100">';
+    domString += `<div class="card-img-top"><img class="image" src=${clips[j].imageUrl} alt="project picture"></div>`;
+    domString += `<div class="card-header"><strong>${clips[j].title}</strong></div>`;
+    domString += '<div class="card-body">';
+    domString += `<div class=mb-2><a href=${clips[j].storyUrl} target="_blank">${clips[j].publication}</a></div>`;
+    domString += `<div>${clips[j].leadText}</div>`;
+    domString += `<div class=mt-2><a href=${clips[j].pdfUrl} target="_blank">Download PDF of full story</i></a></div>`;
+    domString += '</div>';
+    domString += '</div>';
+    domString += '</div>';
+  }
+  domString += '</div>';
+  printToDom('clipsPage', domString);
+};
+
+
 const homePage = () => {
   document.getElementById('bioPage').style.display = 'flex';
   document.getElementById('clipsPage').style.display = 'flex';
@@ -41,9 +63,9 @@ const onlyBio = () => {
   document.getElementById('bioPage').scrollIntoView();
 };
 
-// const onlyClips = () => {
-//   document.getElementById('clipsPage').scrollIntoView();
-// };
+const onlyClips = () => {
+  document.getElementById('clipsPage').scrollIntoView();
+};
 
 const onlyProjects = () => {
   document.getElementById('projectsPage').scrollIntoView();
@@ -56,7 +78,7 @@ const onlyTechs = () => {
 const navEvents = () => {
   document.getElementById('name').addEventListener('click', homePage);
   document.getElementById('navToBio').addEventListener('click', onlyBio);
-  // document.getElementById('navToClips').addEventListener('click', onlyClips);
+  document.getElementById('navToClips').addEventListener('click', onlyClips);
   document.getElementById('navToProjects').addEventListener('click', onlyProjects);
   document.getElementById('navToTechs').addEventListener('click', onlyTechs);
 };
@@ -66,6 +88,11 @@ const init = () => {
   projectsData.getProjects()
     .then((resp) => {
       createProjectCards(resp);
+    })
+    .catch(err => console.error('error from initTypes', err));
+  clipsData.getClips()
+    .then((resp) => {
+      createClipCards(resp);
     })
     .catch(err => console.error('error from initTypes', err));
 };
